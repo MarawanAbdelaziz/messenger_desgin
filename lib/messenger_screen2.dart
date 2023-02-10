f9278017fa8ea0e2e6aac57e708b11f1d6ee14c9
Marawan <Marawan.abdelaziz@icloud.com>
2023-02-10 18:36:18 +0200

    M

--- lib/messenger_screen2.dart
+++ lib/messenger_screen2.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, sized_box_for_whitespace ,prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 class MessengerScreen2 extends StatelessWidget {
